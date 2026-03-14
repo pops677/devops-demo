381c0d046d9cba07647ae54bbf1b0390b756ac2c
pops677 <golubkun@yandex.ru>
2026-03-14 13:23:06 +0300

    fix: initialize counter before loop

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import time
 import sys
 
 def main():
-    print("Hello from Docker!")
-    print(f"Python version: {sys.version}")
+    print("Hello from Docker!", flush=True)
+    print(f"Python version: {sys.version}", flush=True)
     counter = 0
     while True:
-        print(f"alive... ({counter})")
+        print(f"alive... ({counter})", flush=True)
         counter += 1
         time.sleep(10)
 
